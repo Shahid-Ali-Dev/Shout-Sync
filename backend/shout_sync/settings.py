4423ddc750f2d4f141218d3b1445e2063b0cacdf
Shahid Ali <shahid3332210@gmail.com>
2025-11-27 15:48:16 +0530

    Update settings.py

--- backend/shout_sync/settings.py
+++ backend/shout_sync/settings.py
@@ -36,13 +36,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middleware.UpdateLastActiveMiddleware',
 ]
 
 ROOT_URLCONF = 'shout_sync.urls'
